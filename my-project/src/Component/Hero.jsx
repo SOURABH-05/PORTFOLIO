494d5d9ed7cd5f77c16da6356a7c72f101bf5717
Sourabh dharra <s.dharra88@gmail.com>
2025-02-21 11:36:36 +0530

    Your commit message

--- my-project/src/Component/Hero.jsx
+++ my-project/src/Component/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HiOutlineDownload } from "react-icons/hi";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
-import CV from "../assets/Sourabh's CV_compressed.pdf"
+import CV from "../assets/cvs.pdf"
 
 import img from "../assets/IMG_7714.png"
 import { motion } from "framer-motion"
